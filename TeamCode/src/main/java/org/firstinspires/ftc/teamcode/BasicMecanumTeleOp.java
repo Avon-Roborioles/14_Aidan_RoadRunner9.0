a237e6088b8b8e3fcb481c10998a5d685788ba0e
FTCRoborioles <FTCRoborioles2021@gmail.com>
2023-09-23 10:23:54 -0400

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMecanumTeleOp.java
@@ -3,16 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Call_Upon_Classes.BasicMecanumDrive;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.ServoTest;
 
 @TeleOp
+//@Autonomous
 public class BasicMecanumTeleOp extends LinearOpMode {
 
-    //private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.BasicMecanumDrive driveMotors = new BasicMecanumDrive();
+    private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.BasicMecanumDrive driveMotors = new BasicMecanumDrive();
     private final  org.firstinspires.ftc.teamcode.Call_Upon_Classes.ServoTest servo = new ServoTest();
     public void runOpMode() throws InterruptedException {
         //init
-        //driveMotors.init_drive_motors(hardwareMap);
+        driveMotors.init_drive_motors(hardwareMap);
         servo.init_servo(hardwareMap);
         waitForStart();
         while(opModeIsActive()){
